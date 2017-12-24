3184fe4eabb1660c546485640b66e69811d31c1d
blackAndrechen <31908071+blackAndrechen@users.noreply.github.com>
2017-12-24 18:31:06 +0800

    Update crawl_goods.py
    add command func

--- crawl_goods.py
+++ crawl_goods.py
@@ -1,4 +1,3 @@
-import re
 from pyquery import PyQuery as pq
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
@@ -7,32 +6,54 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pymongo
 import pymysql
+import argparse
+import pandas as pd
+import re
 
-#config
-goods = '口红'
-mysqlDB = 'tb'
-mongoDB = 'tb_kouhon'
-mongotable = 'goods_info'
-mysqlpassword = ''
 
-conn = pymysql.connect(host='localhost',
-                       port=3306,
-                       user='root',
-                       password=mysqlpassword,
-                       db=mysqlDB,
-                       charset='utf8mb4')
-cursor = conn.cursor()
+__version__ = "0.2.0"
 
-client = pymongo.MongoClient('localhost',27017)
-dataname = client[mongoDB]
-table = dataname[mongotable]
+#获取命令行参数
+def get_parser():
+    parser = argparse.ArgumentParser(description='search name databasename weatherrank')
+    parser.add_argument('-k','--keyword',type=str,help='you need search taobao goods name')
+    parser.add_argument('-d','--database',type=str,help='saved to database name')
+    parser.add_argument('-v','--version',action='store_true',help='displays the current version of taobao_crawl')
+    return parser
 
-browser = webdriver.Chrome()
-#等待变量
-wait = WebDriverWait(browser,10)
+#解析命令行参数
+def command_line_parser():
+    parser = get_parser()
+    args = vars(parser.parse_args())
+    if args['version']:
+        print(__version__)
+        return
+    if not args['keyword']:
+        parser.print_help()
+    else:
+        #获得要搜索的淘宝商品名字
+        goods = args['keyword']
+        #保存到数据库的名字
+        mongoDB = args['database']
+        mongotable = mongoDB + 'table'
+        #mongodb配置
+        client = pymongo.MongoClient('localhost', 27017)
+        dataname = client[mongoDB]
+        global table
+        table = dataname[mongotable]
+        #启动爬虫
+        sumpage = search(goods)
+        sumpage = int(re.compile('(\d+)').search(sumpage).group(1))  # 利用正则表达式提取数字，并强制转换为int类型
+        for i in range(2, sumpage + 1):
+            next_page(i)
+        browser.close()
 
 #模拟搜索
-def search():
+def search(goods):
+    global browser, wait
+    browser = webdriver.Chrome()
+    #设置等待变量，方便后面代码书写
+    wait = WebDriverWait(browser, 10)
     try:
         browser.get('https://www.taobao.com/')#打开淘宝首页
         tb_input = wait.until(
@@ -41,15 +62,38 @@ def search():
         search_btn = wait.until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_TSearchForm > div.search-button > button'))
         )#等待搜索按钮加载完成
-        tb_input.send_keys(goods)#输入框中传入“美食”
+        tb_input.send_keys(goods)#输入框中传入"商品"
         search_btn.click()#点击搜索
         total = wait.until(
             EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > div.total'))
         )#加载完成，获取页数元素
-        get_products()
+        get_products(page_number=1)
         return total.text#获取元素中的文本
     except TimeoutException:
-        return search()#若发生异常，重新调用自己
+        return search(goods)#若发生异常，重新调用自己
+
+#获取商品信息
+def get_products(page_number):
+    wait.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-itemlist .items .item'))
+    )#等待商品信息加载完成，商品信息的CSS选择器分析HTML源码得到
+    html = browser.page_source#得到页面HTML源码
+    doc = pq(html)#创建PyQuery对象
+    items = doc('#mainsrp-itemlist .items .item').items()#获取当前页所有商品信息的html源码
+    for item in items:
+        product = {
+            # 'image':item.find('.pic .img').attr('src'),
+            'itemid':item.find('.shop .shopname').attr('data-nid'),
+            'sellerid':item.find('.shop .shopname').attr('data-userid'),
+            'url':item.find('.title .J_ClickStat').attr('href'),
+            'price':item.find('.price').text(),
+            'deal':item.find('.deal-cnt').text()[:-3],
+            'title':item.find('.title').text(),
+            'seller':item.find('.shop').text(),
+            'location':item.find('.location').text()
+        }
+        # save_to_mysql(product)
+        save_to_mongoDB(page_number,product)#保存到MongoDB
 
 #翻页函数
 def next_page(page_number):
@@ -66,59 +110,19 @@ def next_page(page_number):
         wait.until(EC.text_to_be_present_in_element(
             (By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > ul > li.item.active > span'), str(page_number))
         )#确认已翻到page_number页
-        get_products()
+        get_products(page_number)
+    # 若发生异常，重新调用自己
     except TimeoutException:
-        next_page(page_number)#若发生异常，重新调用自己
+        next_page(page_number)
 
-#获取商品信息
-def get_products():
-    wait.until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-itemlist .items .item'))
-    )#等待商品信息加载完成，商品信息的CSS选择器分析HTML源码得到
-    html = browser.page_source#得到页面HTML源码
-
-    doc = pq(html)#创建PyQuery对象
-    items = doc('#mainsrp-itemlist .items .item').items()#获取当前页所有商品信息的html源码
-    for item in items:
-        product = {
-            # 'image':item.find('.pic .img').attr('src'),
-            'itemid':item.find('.shop .shopname').attr('data-nid'),
-            'sellerid':item.find('.shop .shopname').attr('data-userid'),
-            'url':item.find('.title .J_ClickStat').attr('href'),
-            'price':item.find('.price').text(),
-            'deal':item.find('.deal-cnt').text()[:-3],
-            'title':item.find('.title').text(),
-            'seller':item.find('.shop').text(),
-            'location':item.find('.location').text()
-        }
-        print(product)
-        # save_to_mysql(product)
-        # save_to_mongoDB(product)#保存到MongoDB
-
-#保存到MongoDB
-def save_to_mongoDB(product):
+def save_to_mongoDB(page_number,product):
     try:
         if table.insert(product):
-            print('存储到MongoDB成功',product)
+            print("saved {} page --->".format(page_number), product['title'])
     except Exception:
-        print('存储到MongoDB失败',product)
-
-def save_to_mysql(product):
-    sql = '''INSERT INTO tb_inf(itemid,sellerid,url,price,deal,title,seller,location) VALUES (%s,%s,"%s","%s",%s,"%s","%s","%s")'''
-    try:
-        cursor.execute(sql % (product['itemid'],product['sellerid'],product['url'],product['price'],product['deal'],product['title'],product['seller'],product['location']))
-        conn.commit()
-        print('save to mysql successed',product)
-    except:
-        conn.rollback()
-
-def main():
-    total = search()#获取商品页数，字符串类型
-    total = int(re.compile('(\d+)').search(total).group(1))#利用正则表达式提取数字，并强制转换为int类型
-    for i in range(2, total+1):
-        next_page(i)
-    conn.close()
-    browser.close()
+        print('save {} page failed'.format(page_number), product)
 
 if __name__ == '__main__':
-    main()
+    command_line_parser()
+
+
